2f725e0dec6dfb2849e738af548f0c77dd4fb2ab
Taoyuan Zhang <tyuan0@gmail.com>
2024-05-07 23:20:24 -0400

    completed ... I think

---
+++ src/constants.js
@@ -0,0 +1,9 @@
+export const scaleFactor = 4;
+
+export const dialogueData = {
+    pc: `This is my PC.`,
+    tv: `This is my TV.`,
+    bed: `Rest here.`,
+    resume: `My resume in progress.`,
+    projects: `This is something I'm working on.`,
+}
